89b7f22f55bbf99c7f2dc6c022eb80035a0d0294
n0127 <110751236+n0127@users.noreply.github.com>
2023-01-18 17:16:03 +0800

    fix
    fix:1

---
+++ down/js/channelback60.js
@@ -0,0 +1,9 @@
+var channelback={
+    
+    "xgysvip":{"addr":"https://cowtransfer.com/s/1cda5dacbe7a47"},
+    "km":{"addr":"https://cowtransfer.com/s/5e3c2abd3f4446"},
+    "bzs":{"addr":"https://cowtransfer.com/s/9747aa4dedd148"},
+     "hp":{"addr":"https://cowtransfer.com/s/ba66b7aef43b4c"}
+    
+    
+     }
