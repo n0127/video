4f06234d213282f406f9085c6f9de39fc4d79592
ness <ness@nescom>
2023-04-12 17:08:40 +0800

    d

--- ldy/share/js/channel60.js
+++
@@ -1,8 +0,0 @@
-var channel={
-    
-    "xgysvip":{"addr":"ixAgL05dhrud","pwd":"2b8f"},
-    "km":{"addr":"iuhK405dhrha","pwd":"b6n4"},
-    "bzs":{"addr":"i63aZ05dflbg","pwd":"3cvb"},
-     "hp":{"addr":"iHhCy05dhr7a","pwd":"294u"}
-    
-     }
